81981d79b619db92cf57ce59105f58db1506e65f
Mathias Burger <mathias.burger89@gmail.com>
2021-10-28 19:47:44 +0200

    Create my first full-stack app

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import { useState } from 'react';
-import { albumList, artistList, genreList } from '../assets/additionalAssets';
-import { songList } from '../assets/songList';
 import { AudioPlayer } from '../components/AudioPlayer';
 import { FilterInput } from '../components/FilterInput';
 
-export default function Home() {
+export default function Home({ albums, artists, genres, songs }) {
   const [genreFilter, setGenreFilter] = useState('');
   const [artistFilter, setArtistFilter] = useState('');
   const [albumFilter, setAlbumFilter] = useState('');
-  const [activeSong, setActiveSong] = useState(songList[0]);
+  const [activeSong, setActiveSong] = useState();
+
   return (
     <div>
       <header>
@@ -17,24 +16,24 @@ export default function Home() {
       </header>
       <AudioPlayer activeSong={activeSong} />
       <section className="filter-section">
-      <FilterInput
-    options={genreList} // genreList from assets directory
-    value={genreFilter} // genreFilter from useState()
-    filterSetter={setGenreFilter} // setGenreFilter from useState()
-    name="genre"
-    />
-      <FilterInput
-          options={artistList} // artistList from assets directory
-          value={artistFilter} // artistFilter from useState()
-          filterSetter={setArtistFilter} // setArtistFilter from useState()
+        <FilterInput
+          options={genres}
+          value={genreFilter}
+          filterSetter={setGenreFilter}
+          name="genre"
+        />
+        <FilterInput
+          options={artists}
+          value={artistFilter}
+          filterSetter={setArtistFilter}
           name="artist"
         />
-      <FilterInput
-          options={albumList} // albumList from assets directory
-          value={albumFilter} // albumFilter from useState()
-          filterSetter={setAlbumFilter} // setAlbumFilter from useState()
+        <FilterInput
+          options={albums}
+          value={albumFilter}
+          filterSetter={setAlbumFilter}
           name="album"
-  />
+        />
       </section>
       <div className="song-list">
         <div className="song-header">
@@ -45,67 +44,64 @@ export default function Home() {
           <div>play</div>
           <div>genre</div>
         </div>
-        {songList
+        {songs
           .filter((song) => {
-            let isVisible = true; // by default all songs are visible
-        
-            // if there is a genre selected and this dont match to the genre of this song hide the song
+            let isVisible = true;
             if (genreFilter && genreFilter !== song.genre) {
               isVisible = false;
             }
-             // if there is a genre selected and this dont match to the genre of this song hide the song
-             if (artistFilter && artistFilter !== song.artist) {
+            if (artistFilter && artistFilter !== song.artist) {
               isVisible = false;
             }
-             // if there is a genre selected and this dont match to the genre of this song hide the song
-             if (albumFilter && albumFilter !== song.album) {
+            if (albumFilter && albumFilter !== song.album) {
               isVisible = false;
             }
-        
-            // Apply the same pattern for artists and albums
-        
             return isVisible;
-          }).map((song) => {
-          return (
-            <div
-              key={song.id}
-              className="song-container"
-              onDoubleClick={() => {
-                setActiveSong(song);
-              }}
-            >
-              <div className="song-title">{song.name}</div>
-              <div>{song.album}</div>
-              <div>{song.release}</div>
-              <div>{song.artist}</div>
-              <button
-                className="song-play"
-                onClick={() => {
+          })
+          .map((song) => {
+            return (
+              <div
+                key={song.id}
+                className="song-container"
+                onDoubleClick={() => {
                   setActiveSong(song);
                 }}
               >
-                <img src="/play.svg" alt="play" />
-              </button>
-              <div>{song.genre}</div>
-            </div>
-          );
-        })}
+                <div className="song-title">{song.name}</div>
+                <div>{song.album}</div>
+                <div>{song.release}</div>
+                <div>{song.artist}</div>
+                <button
+                  className="song-play"
+                  onClick={() => {
+                    setActiveSong(song);
+                  }}
+                >
+                  <img src="/play.svg" alt="play" />
+                </button>
+                <div>{song.genre}</div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
 }
 
-// ⚠️⚠️⚠️
-// ⚠️⚠️⚠️ PLEASE IGNORE this section until you have completed steps  1, 2 and 3 ⚠️⚠️⚠️
-// ⚠️⚠️⚠️
-//
-// --------------- Step 4 --------------
-//
-// Now, we are going to connect our front-end and back-end.
-// Using Get Server Side Props (aka. GSSp) get the app data from the database instead of the assets files.
-//
-// 1. Import the database Util functions from utils/database.js
-// 2. Get the data for the app. (albumList, artistList, genreList, songList)
-// 3. Pass the data to the Page Component through props object
-// 4. modify the code in the Home component to receive the data from database instead of assets
-// 5. remove assets directory and the files inside
+export async function getServerSideProps() {
+  const { getAlbums, getArtists, getGenres, getSongs } = await import(
+    '../utils/database'
+  );
+  const albums = await getAlbums();
+  const artists = await getArtists();
+  const genres = await getGenres();
+  const songs = await getSongs();
+  return {
+    props: {
+      albums,
+      artists,
+      genres,
+      songs,
+    },
+  };
+}
